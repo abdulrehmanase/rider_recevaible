c1bb0ea705843b817a6364625887180a2137735f
abdulrehmanase <abdul.rehman.ase@cheetay.pk>
2021-10-21 12:18:24 +0500

    first commit

--- rider_shift_lambda.py
+++ rider_shift_lambda.py
@@ -1,35 +1,107 @@
 from utils import *
-from sql import rider_shifts_sql
+from sql import *
+from datetime import datetime, timedelta
 
 ID = 'ID'
-SHIFT_START_DATE = 'Shift Start Date'
-SHIFT_START_TIME = 'Shift Start Time'
-SHIFT_END_DATE = 'Shift End Date'
-SHIFT_END_TIME = 'Shift End Time'
-HOT_SPOT = 'HotSpot'
-CITY = 'City'
-RIDER_REQUIRED = 'Rider Required'
+NAME = 'Name'
+MOBILE_NUMBER = 'Mobile Number'
+NIC = 'CNIC'
+RIDER_CITY = 'Rider City'
+EQUIPMENT_COST = 'Equipment Cost'
+PICKUP_BONUS = 'Pickup Bonus (PB)'
+PICKUP_DISTANCE = "Pickup Distance KM"
+DROP_OFF_DISTANCE_PAY = 'Drop-off Pay (DDP)'
+DROP_OFF_DISTANCE = "Drop-off Distance KM"
+RECEIVABLE_AMOUNT = 'Receivable Amount'
+FUEL_ALLOWANCE = "Fuel Allowance"
+FINAL_RECEIVABLE_AMOUNT = "Final Receivable Amount"
+LAST_SETTLEMENT_DATE = 'Last Settlement Date'
+DATE_LAST_SETTLEMENT = 'Date'
+TIME_LAST_SETTLEMENT = 'Time'
 
 
-def rider_fill_rate(start_date, end_date):
-    connection = connect_to_db()
-    cursor = connection.cursor()
-    cursor.execute(rider_shifts_sql(start_date, end_date))
+def rider_receivables(start_date, end_date):
+    riders_data = []
+    riders = get_data()
 
-    shifts = cursor.fetchall()
+    for rider in riders:
+        report_last_settlement = ''
+        report_last_settlement_date = ''
+        report_last_settlement_time = ''
+        last_settlement = last_settlement_query(end_date, rider[0])
 
-    riders_data = [{ID: shift[0], SHIFT_START_DATE:shift[1], SHIFT_START_TIME: shift[2], SHIFT_END_DATE:shift[3],
-                    SHIFT_END_TIME:shift[4], HOT_SPOT:shift[5], CITY: shift[6], RIDER_REQUIRED:shift[7]}
-                   for shift in shifts]
 
-    header = [ID, SHIFT_START_DATE, SHIFT_START_TIME, SHIFT_END_DATE, SHIFT_END_TIME, HOT_SPOT, CITY, RIDER_REQUIRED]
+        if last_settlement:
+            report_last_settlement = str(last_settlement[0][1])
+            print('report',report_last_settlement)
+            last_settlement_date = str(last_settlement[0][1].date())
+            report_last_settlement_time = str(last_settlement[0][1].time())
+            report_last_settlement_date = last_settlement_date
+            # start and end times for filter earnings
+            data = get_dates( last_settlement_date, end_date)
+            start_time, end_time = report_last_settlement, data['end_time']
+        else:
+            config = logistics_configuration_instance()
+            # start and end times for filter earnings
+            data = get_dates( str(config[0]), end_date)
+            start_time, end_time = data['start_time'], data['end_time']
 
-    file_name = 'Rider Fill Rate.csv'
+        rc_sum = rc_sum_query(end_date, rider[0])
+        print('rc_sum', rc_sum)
+        trans_type_c = rc_sum[0][0] or 0
+        trans_type_d = rc_sum[0][1] or 0
+        sum_total = trans_type_c - trans_type_d
+        receivable_amount = rider[1] - sum_total
+        equipment_cost = get_equipment_cost(rider[0], start_time, end_time)
+        print('equipment', equipment_cost)
+        pickup_distances = get_rider_pickup_distances(rider[0], start_time,
+                                                      end_time, log_type="PB")
+        pickup_distance = float(pickup_distances)
+        print('pickup', pickup_distance)
+        delivered_distances = get_rider_drop_off_distances(rider[0], start_time,
+                                                           end_time, log_type="DDP")
+        delivered_distance = float(delivered_distances)
+        print('del',delivered_distance)
+        earnings_data = get_rider_earnings(rider[0], start_time, end_time)
+        pick_up_distance_bonus = earnings_data[0][0] or 0
+        drop_off_distance_pay = earnings_data[0][2] or 0
+        print('dropoff', drop_off_distance_pay)
+        fuel_allowance = pick_up_distance_bonus + drop_off_distance_pay
+        final_receivable_amount = receivable_amount - fuel_allowance
+        if final_receivable_amount >= 1:
+            riders_data.append({ID: rider[0], NAME: rider[2], MOBILE_NUMBER: rider[4],
+                                RIDER_CITY: rider[3], NIC: rider[5],
+                                EQUIPMENT_COST: equipment_cost,
+                                PICKUP_BONUS: pick_up_distance_bonus,
+                                PICKUP_DISTANCE: pickup_distance,
+                                DROP_OFF_DISTANCE_PAY: drop_off_distance_pay,
+                                DROP_OFF_DISTANCE: delivered_distance,
+                                RECEIVABLE_AMOUNT: receivable_amount,
+                                FUEL_ALLOWANCE: fuel_allowance,
+                                FINAL_RECEIVABLE_AMOUNT: final_receivable_amount,
+                                LAST_SETTLEMENT_DATE: report_last_settlement,
+                                DATE_LAST_SETTLEMENT: report_last_settlement_date,
+                                TIME_LAST_SETTLEMENT: report_last_settlement_time
+                                })
+    header = [ID, NAME, MOBILE_NUMBER, NIC, RIDER_CITY, EQUIPMENT_COST, PICKUP_BONUS, PICKUP_DISTANCE,
+              DROP_OFF_DISTANCE_PAY, DROP_OFF_DISTANCE, RECEIVABLE_AMOUNT, FUEL_ALLOWANCE, FINAL_RECEIVABLE_AMOUNT,
+              LAST_SETTLEMENT_DATE, DATE_LAST_SETTLEMENT, TIME_LAST_SETTLEMENT]
+
+    title = 'Rider Receivables Report - {}'.format(end_date)
+    file_name = '{}.csv'.format(title)
     zip_file = create_csv(file_name, riders_data, header)
     attachments = [{'name': file_name + '.zip', 'content': zip_file.getvalue()}]
-    title = 'Rider Fill Report'
-    print(zip_file)
+    import csv
 
 
-rider_fill_rate("2019-10-10", "2020-10-10")
+
+    with open('countries.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(header)
+
+        # write the data
+        writer.writerow(riders_data)
+rider_receivables("2019-10-10", "2020-10-10")
 
--- sql.py
+++ sql.py
@@ -1,11 +1,121 @@
+from utils import *
 
-def rider_shifts_sql(start_date, end_date):
-    sql_query = (""" SELECT shift.id AS ID ,DATE_FORMAT(shift.start_at , '%Y-%m-%d') ,
-                    DATE_FORMAT(shift.start_at ,'%H:%i:%s') , DATE_FORMAT(shift.close_at , '%Y-%m-%d') ,
-                    DATE_FORMAT(shift.close_at ,'%H:%i:%s') ,h.area_name AS HOT_SPOT,c.name AS city ,shift.riders_required AS RIDER_REQUIRED FROM
-                    (shift
-                     INNER JOIN  hotspot h
-                     ON shift.hotspot_id = h.id)
-                     LEFT JOIN city c ON h.city_id = c.id where shift.start_at< '{}' and shift.close_at>'{}'""".format(
-        end_date, start_date))
-    return sql_query
+
+def get_data():
+    get_data_query = ("""SELECT r.id, r.cash_in_hand,CONCAT(au.first_name,'',au.last_name) as ridername ,c.name,r.mobile_number ,r.nic FROM rider r INNER JOIN city c ON (r.city_id = c.id) INNER JOIN auth_user au ON 
+                    (r.user_id = au.id) WHERE (r.cash_in_hand > 0.0 AND r.city_id IS NOT NULL AND au.is_active = True) limit 5 """)
+
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(get_data_query)
+    get_query = cursor.fetchall()
+    return get_query
+
+
+def last_settlement_query(end_date,rider):
+    last_settlement_sql = ("""SELECT rc.id ,rc.created_at FROM rider_cash rc 
+                            WHERE (DATE(CONVERT_TZ(rc.created_at , 'UTC', 'UTC')) <= '{}' AND rc.log_type 
+                            IN (4, 5) AND rc.rider_id = '{}') ORDER BY rc.id DESC limit 1""".format(end_date, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(last_settlement_sql)
+    settlement_query = cursor.fetchall()
+    return settlement_query
+
+
+def logistics_configuration_instance():
+
+    LogisticsConfiguration_instance_sql = ("""select lc.enforce_fuel_deduction_date from logistics_configuration lc limit 1""")
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(LogisticsConfiguration_instance_sql)
+    instance = cursor.fetchall()
+    return instance[0]
+
+
+def rc_sum_query(end_date, rider):
+    rc_sum_query_sql = ("""SELECT sum(case when rc.trans_type = 'c' then amount  end) as trans_type_c,
+                        sum(case when rc.trans_type = 'd' then amount  end) as trans_type_d
+                        FROM rider_cash rc WHERE (DATE(CONVERT_TZ(rc.created_at , 'UTC', 'UTC')) > '{}'
+                        AND rc.log_type IN (1, 2, 3) AND rc.rider_id = '{}')""".format(end_date, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(rc_sum_query_sql)
+    rc_sum_sql = cursor.fetchall()
+    return rc_sum_sql
+
+
+def get_equipment_cost(rider, start_time, end_time):
+    get_equipment_cost_sql = ("""SELECT sum(rel.cost) as amount FROM rider_equipment_log rel WHERE (rel.created_at 
+                                BETWEEN '{}' AND '{}' AND rel.rider_id = '{}')""".format(start_time, end_time, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(get_equipment_cost_sql)
+    equipment_cost = cursor.fetchall()
+    return equipment_cost[0][0] or 0
+
+
+def get_rider_pickup_distances(rider, start_time, end_time, log_type):
+    pick_up_distance_sql = ("""select  sum(od.pickup_distance) from `order`o right join rider_earnings re 
+                    on o.id=re.order_id
+                    inner join order_distance od 
+                    on od.order_id = o.id  
+                    WHERE (re.created_at BETWEEN '{}' AND 
+                    '{}') AND 
+                    re.log_type ='{}' and
+                    re.rider_id='{}'
+                    """.format(start_time, end_time, log_type, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(pick_up_distance_sql)
+    pick_up_distance = cursor.fetchall()
+    if pick_up_distance[0][0]:
+        return pick_up_distance[0][0]
+    return 0
+
+
+def get_rider_drop_off_distances(rider, start_time, end_time, log_type):
+
+    drop_off_distance_sql = (""" select sum(od.delivered_distance) from `order`o right join rider_earnings re 
+                                on o.id=re.order_id
+                                inner join order_distance od 
+                                on od.order_id = o.id  
+                                WHERE (re.created_at BETWEEN '{}' AND 
+                                '{}') AND 
+                                re.log_type ='{}' and
+                                re.rider_id='{}'
+                                 """.format(start_time, end_time, log_type, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(drop_off_distance_sql)
+    drop_off_distance = cursor.fetchall()
+    if drop_off_distance[0][0]:
+        return drop_off_distance[0][0]
+    return 0
+
+
+def get_rider_earnings(rider, start_time, end_time):
+    rider_earnings_sql = ("""SELECT  
+                        SUM(CASE when re.log_type="PB" then amount  END) as pick_up_distance_bonus ,
+                        SUM(CASE when re.log_type="PP" then amount  END) as pick_up_pay ,
+                        SUM(CASE when re.log_type="DDP" then amount  END) as drop_off_distance_pay,
+                        SUM(CASE when re.log_type="DP" then amount  END) as drop_off_pay,
+                        SUM(CASE when re.log_type="DCP" then amount  END) as delivery_charges_based_pay,
+                        SUM(CASE when re.log_type="FP" then amount  END) as per_order_pay,
+                        SUM(CASE when re.log_type="SBP" then amount  END) as slab_based_pay,
+                        SUM(CASE when re.log_type="TP" then amount  END) as tip_pay,
+
+                        COUNT(case when re.log_type="PP" then order_id  END) total_pick_ups,
+                        COUNT(case when re.log_type="DP" then order_id  END) total_drop_offs,
+                        COUNT(case when re.log_type="FP" then order_id  END) total_per_order_pays,
+                        COUNT(case when re.log_type="SBP" then order_id  END) total_slab_based_pays,
+                        SUM(CASE when re.log_type="LNB" then amount  END) as total_late_night_bonus_pay
+                        from rider_earnings re  where re.created_at BETWEEN '{}'
+                        AND '{}'
+                        AND re.rider_id ='{}' 
+                        """.format(start_time, end_time, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(rider_earnings_sql)
+    drop_off_distance = cursor.fetchall()
+    return drop_off_distance
